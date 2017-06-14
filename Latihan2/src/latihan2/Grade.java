eb179e03f25ed4ee713437d14ce3e2ebf7a46ddc
Ghazi Fadil <ghazifardhan@gmail.com>
2017-06-14 08:27:26 +0700

    add

---
+++ Latihan2/src/latihan2/Grade.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author cube
+ */
+public class Grade {
+    
+    private Integer nilaiKehadiran;
+    private Integer nilaiTugas;
+    private Integer nilaiUts;
+    private Integer nilaiUas;
+
+    public Integer getNilaiKehadiran() {
+        return nilaiKehadiran;
+    }
+
+    public void setNilaiKehadiran(Integer nilaiKehadiran) {
+        this.nilaiKehadiran = nilaiKehadiran;
+    }
+
+    public Integer getNilaiTugas() {
+        return nilaiTugas;
+    }
+
+    public void setNilaiTugas(Integer nilaiTugas) {
+        this.nilaiTugas = nilaiTugas;
+    }
+
+    public Integer getNilaiUts() {
+        return nilaiUts;
+    }
+
+    public void setNilaiUts(Integer nilaiUts) {
+        this.nilaiUts = nilaiUts;
+    }
+
+    public Integer getNilaiUas() {
+        return nilaiUas;
+    }
+
+    public void setNilaiUas(Integer nilaiUas) {
+        this.nilaiUas = nilaiUas;
+    }
+    
+    
+}
